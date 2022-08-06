39395b309bd4531b9d413758ccccea874db36570
bobubear <jedsa.pd@gmail.com>
2022-08-06 22:00:00 +0700

    finish

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# lab 05 : HTML JavaScript DOM Programming 
+# lab 05 : HTML JavaScript DOM Programming
 
 หลังจากน.ศ. clone repo แล้ว ให้ทำตามขั้นตอนดังนี้
 
@@ -7,5 +7,5 @@
 https://o365cmu-my.sharepoint.com/:b:/g/personal/chayanin_s_cmu_ac_th1/EaPu0VTki21FvlzSm7qULtoBpiYzYO8Gu5Jg2YWXesY3fA?e=bSiLfc
 
 ใส่ GitHub page URL ด้านล่างนี้
-
+https://cpe-cmu-261207.github.io/lab-05-buboobear/
 [YOUR GITHUB PAGE URL]
--- script.js
+++ script.js
@@ -2,47 +2,87 @@ const inputAdd = document.getElementById("input-add-todo");
 const todoCtn = document.getElementById("todo-container");
 
 inputAdd.onkeyup = (event) => {
-  if (event.key !== "Enter") return;
+    if (event.key !== "Enter") return;
 
-  //your code here
+    //your code here
+    if (inputAdd.value === "") {
+        alert("Todo cannot be empty");
+    } else {
+        addTodo(inputAdd.value, false);
+        saveTodo();
+        inputAdd.value = "";
+    }
 };
 
 function addTodo(title, completed) {
-  //create a div that holds todo title, done button, delete button
-  const div = document.createElement("div");
-  div.className = "border-bottom p-1 py-2 fs-2 d-flex";
+    //create a div that holds todo title, done button, delete button
+    const div = document.createElement("div");
+    div.className = "border-bottom p-1 py-2 fs-2 d-flex";
 
-  //create span for showing title
-  const span = document.createElement("span");
-  span.innerText = title;
-  span.style.textDecoration = completed ? "line-through" : "";
-  span.className = "me-3";
+    //create span for showing title
+    const span = document.createElement("span");
+    span.innerText = title;
+    span.style.textDecoration = completed ? "line-through" : "";
+    span.className = "me-3";
 
-  //create done button
-  const doneBtn = document.createElement("button");
-  doneBtn.innerText = "Done";
-  doneBtn.className = "btn btn-success me-2";
+    //create done button
+    const doneBtn = document.createElement("button");
+    doneBtn.innerText = "Done";
+    doneBtn.className = "btn btn-success me-2";
 
-  //create delete button
-  const deleteBtn = document.createElement("button");
-  deleteBtn.innerText = "Delete";
-  deleteBtn.className = "btn btn-danger";
+    //create delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.className = "btn btn-danger";
 
-  //your code here
-  //append todo to HTML...
-  //define buttons event...
+    //your code here
+    todoCtn.prepend(div);
+    div.append(span);
+    div.append(doneBtn);
+    div.append(deleteBtn);
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+    div.onmouseover = () => {
+        doneBtn.style.display = "";
+        deleteBtn.style.display = "";
+    };
+    div.onmouseout = () => {
+        doneBtn.style.display = "none";
+        deleteBtn.style.display = "none";
+    };
+    doneBtn.onclick = () => {
+        completed = !completed;
+        span.style.textDecoration = completed ? "line-through" : "";
+        saveTodo();
+    };
+    deleteBtn.onclick = () => {
+        todoCtn.removeChild(div);
+        saveTodo();
+    };
+    //append todo to HTML...
+    //define buttons event...
 }
 
 function saveTodo() {
-  const data = [];
-  for (const todoDiv of todoCtn.children) {
+    const data = [];
+    for (const todoDiv of todoCtn.children) {
+        //your code here
+        const todoObj = {};
+        todoObj.title = todoDiv.children[0].innerText;
+        todoObj.completed =
+            todoDiv.children[0].style.textDecoration === "line-through";
+        data.push(todoObj);
+    }
     //your code here
-  }
-  //your code here
+    localStorage.setItem("key", JSON.stringify(data));
 }
 
 function loadTodo() {
-  //your code here
+    //your code here
+    const data = JSON.parse(localStorage.getItem("key"));
+    for (let i = data.length - 1; i >= 0; i--) {
+        addTodo(data[i].title, data[i].completed);
+    }
 }
 
 loadTodo();
